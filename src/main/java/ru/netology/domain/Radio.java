aef7c0f824ac0e7273e390bd219f927f9ad7be97
Andrey Malov <maa.pers@gmail.com>
2022-05-27 13:18:37 +0200

    Radio methods and tests added

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,86 @@
 package ru.netology.domain;
 
 public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+// Выбор и настройка радиостанций:
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void next() {
+        if (currentStation < 9) {
+            currentStation = currentStation + 1;
+        }
+    }
+
+    public void changeOver(int currentStation) {
+        if (currentStation == 9) {
+            this.currentStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        }
+    }
+
+    public void changeBackOver(int currentStation) {
+        if (currentStation == 0) {
+            this.currentStation = 9;
+        }
+    }
+
+
+// Выбор и настройка уровня громкости:
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void maxVolume(int currentVolume) {
+        if (currentVolume == 10) {
+            this.currentStation = 10;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void minVolume(int currentVolume) {
+        if (currentVolume == 0) {
+            this.currentStation = 0;
+        }
+    }
 
 }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,4 +1,76 @@
 package ru.netology.domain;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class RadioTest {
+
+// Тестирование выбора и настройки радиостанций:
+    @ParameterizedTest
+    @CsvSource({"5,5", "1,1", "9,9", "-1,0", "0,0", "10,0"})
+    void shouldTuneStation(int currentStation, int expected) {
+        Radio station = new Radio();
+        station.setCurrentStation(currentStation);
+        int actual = station.getCurrentStation();
+        assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,1", "1,2", "5,6", "9,0"})
+    void shouldTuneStationUpAndOver(int currentStation, int expected) {
+        Radio tuneUp = new Radio();
+        tuneUp.setCurrentStation(currentStation);
+        tuneUp.next();
+        tuneUp.changeOver(currentStation);
+        int actual = tuneUp.getCurrentStation();
+        assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,9", "9,8", "5,4", "1,0"})
+    void shouldTuneStationDownAndBackOver(int currentStation, int expected) {
+        Radio tuneDown = new Radio();
+        tuneDown.setCurrentStation(currentStation);
+        tuneDown.prev();
+        tuneDown.changeBackOver(currentStation);
+        int actual = tuneDown.getCurrentStation();
+        assertEquals(actual, expected);
+    }
+
+
+// Тестирование выбора и настройки уровня громкости:
+    @ParameterizedTest
+    @CsvSource({"7,7", "1,1", "10,10", "-1,0", "0,0", "11,0"})
+    void shouldTuneVolume(int currentVolume, int expected) {
+        Radio volume = new Radio();
+        volume.setCurrentVolume(currentVolume);
+        int actual = volume.getCurrentVolume();
+        assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,1", "1,2", "5,6", "10,10"})
+    void shouldIncreaseVolumeToMax(int currentVolume, int expected) {
+        Radio volumeUp = new Radio();
+        volumeUp.setCurrentVolume(currentVolume);
+        volumeUp.increaseVolume();
+        volumeUp.maxVolume(currentVolume);
+        int actual = volumeUp.getCurrentVolume();
+        assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"10,9", "6,5", "1,0", "0,0"})
+    void shouldDecreaseVolumeToMin(int currentVolume, int expected) {
+        Radio volumeDown = new Radio();
+        volumeDown.setCurrentVolume(currentVolume);
+        volumeDown.decreaseVolume();
+        volumeDown.minVolume(currentVolume);
+        int actual = volumeDown.getCurrentVolume();
+        assertEquals(actual, expected);
+    }
+
 }
